b0f9b9fbb0bcb359c64321d99d07cf4db90846b4
charliepan <hsinchih.pan@gmail.com>
2020-03-05 15:02:51 -0600

    Added code for callbacks-1

--- js-competencies/callbacks-1/callbacks.js
+++ js-competencies/callbacks-1/callbacks.js
@@ -3,7 +3,19 @@
 // Write a function called stringItOut that takes in an array. Determine if it is an array of numbers or strings and sort it greatest to least accordingly (sort strings alphabetically). Return the array converted into a string using the toString method.
 
 //Code here
+const stringItOut = arr =>{
+  if(typeof arr[0] === 'string'){
+    arr.sort();
+    arr.reverse();
+  }
+  else{
+    arr.sort((a,b)=> b-a);
+  }
+  
+  let str = arr.toString();
 
+  return str;
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -18,3 +30,4 @@ function complex(num1, num2) {
 }
 
 //Code here
+const mathResult = math(complex,2,3);
